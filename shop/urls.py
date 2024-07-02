90d5b4b9773985336a372964fbdd888045a53649
Alexandrov Vladislav <zxcqwe1954@mail.ru>
2024-07-02 13:25:54 +0300

    site search

--- shop/urls.py
+++ shop/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import products_view, product_detail
+from .views import products_view, product_detail, search_view
 
+# from . import views 
 
 app_name = 'shop'
 
 urlpatterns = [
     path('', products_view, name='products'),
+    path('search/', search_view, name='search'),
     path('<slug:slug>/', product_detail, name='product_detail'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Category, ProductProxy
+from .models import Category, ProductProxy, Product
+from django.views.generic import ListView
+
+from django.db.models import Q
 
 
 def products_view(request):
@@ -10,3 +13,14 @@ def products_view(request):
 def product_detail(request, slug):
     product = get_object_or_404(ProductProxy, slug=slug)
     return render(request, 'shop/product_detail.html', {'product': product})
+
+
+def search_view(request):
+    query = request.GET.get('q')
+    if query:
+        products = Product.objects.filter(
+            Q(title__icontains=query))
+    else:
+        products = ProductProxy.objects.none()
+    return render(request, 'shop/products.html', {'products': products})
+
