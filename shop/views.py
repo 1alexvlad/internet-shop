d8b021885d27020284ceb6e814d39df27056434d
Alexandrov Vladislav <zxcqwe1954@mail.ru>
2024-07-03 13:48:51 +0300

    homepage filtering

--- shop/views.py
+++ shop/views.py
@@ -7,12 +7,22 @@ from django.core.paginator import Paginator
 
 
 def products_view(request):
-    products = ProductProxy.objects.all().order_by('id')  
-    
-    paginator = Paginator(products, 4)  # Показывать 4 продуктов на странице
+    products = ProductProxy.objects.all()
+    sort_by = request.GET.get('sort', 'default')
+
+    if sort_by == 'asc':
+        products = products.order_by('price')
+    elif sort_by == 'desc':
+        products = products.order_by('-price')
+    else:
+        products = products.order_by('id')
+
+    paginator = Paginator(products, 4)
     page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)     
-    return render(request, 'shop/products.html', {'page_obj': page_obj}) 
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'shop/index.html', {'page_obj': page_obj, 'sort_by': sort_by})
+
 
 
 def product_detail(request, slug):
@@ -22,18 +32,24 @@ def product_detail(request, slug):
 
 def search_view(request):
     query = request.GET.get('q')
+    sort_by = request.GET.get('sort', 'default')
+
     if query:
         products = ProductProxy.objects.filter(
             Q(title__icontains=query))
     else:
-        products = ProductProxy.objects.none()
+        products = ProductProxy.objects.all()
 
-    paginator = Paginator(products, 4) 
+    if sort_by == 'asc':
+        products = products.order_by('price')
+    elif sort_by == 'desc':
+        products = products.order_by('-price')
+    else:
+        products = products.order_by('id')
+
+    paginator = Paginator(products, 4)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
 
-    return render(request, 'shop/products.html', {'page_obj': page_obj, 'query': query})
-
-
-
+    return render(request, 'shop/search.html', {'page_obj': page_obj, 'query': query, 'sort_by': sort_by})
 
