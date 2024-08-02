4f411d7c9e475b590c521533ba4164249789eba7
Alexandrov Vladislav <zxcqwe1954@mail.ru>
2024-08-02 23:03:11 +0300

    postgresql

--- website/settings.py
+++ website/settings.py
@@ -79,10 +79,19 @@ WSGI_APPLICATION = 'website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'shop-internet',
+        'USER': os.environ.get('USER'),
+        'PASSWORD': os.environ.get('PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '5432',
+        'OPTIONS': {
+            'connect_timeout': 5,
+        },
     }
 }
+
+
 
 CACHES = {
     "default": {
